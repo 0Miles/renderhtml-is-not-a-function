3822e617871ea5142e6409abfd9272188f155a23
Miles <git@miles.tw>
2023-12-08 22:00:40 +0800

    master

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import { CSSProvider } from '@master/css.react'
+import config from '../master.css'
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +14,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+      <html lang="en" style={process.env.NODE_ENV === 'development' ? { display: 'none' } : undefined}>
+          <body>
+              <CSSProvider config={config}>
+                  {children}
+              </CSSProvider>
+          </body>
+      </html>
   )
 }
